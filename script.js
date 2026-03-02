92fcb4678bc109fffc920a9d130ff89382c28779
ixnawg <ixnawg@gmail.com>
2026-03-02 15:51:21 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("scroll", function(){
-    const header = document.querySelector("header");
-    header.style.boxShadow = window.scrollY > 50
-        ? "0 5px 20px rgba(0,0,0,0.1)"
-        : "none";
-});
